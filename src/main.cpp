3271453d69350957bd776761d85aa1be0fd82ccf
amayaleo <amayaleod@yahoo.com>
2023-09-19 18:47:21 -0300

    update 20230919

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,9 @@ int main(void){
   
    try{
         ECS::EntityManager_t entityMan;
-        entityMan.createEntity(320,480,16,16,0x00ffffff);
-        //entityMan.createEntity(80,10,"assets/ninja.png");
+        entityMan.createEntity(320,240,16,16,0x00ff00ff);
+
+        entityMan.createEntity(80,10,"assets/ninja.png");
         //entityMan.createEntity(200,70,"assets/ninja.png");
 
         const ECS::RenderSystem_t render{ kSCRWIDTH , kSCRHEIGHT , entityMan };
@@ -42,6 +43,7 @@ int main(void){
    catch(...){
      std::cout<< "break program"<<std::endl;
    }
+
 return 0;
 }
 
